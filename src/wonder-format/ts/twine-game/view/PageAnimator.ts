c575c347b16e44f137707682abab7fd0e63476af
KvisazGames <kiko@kiko.kiko>
2020-05-10 14:06:52 +0700

    0.6.19 - PageAnimator cleaning

--- src/wonder-format/ts/twine-game/view/PageAnimator.ts
+++ src/wonder-format/ts/twine-game/view/PageAnimator.ts
@@ -2,12 +2,12 @@ import {COMMON_CSS} from "../../Constants";
 
 export class PageAnimator {
     show(el: Element, duration = 0) {
-        console.log(`show el`, el);
+       // console.log(`show el`, el);
         el.classList.remove(COMMON_CSS.displayNone);
     }
 
     hide(el: Element, duration = 0) {
-        console.log(`hide el`, el);
+     //   console.log(`hide el`, el);
         el.classList.add(COMMON_CSS.displayNone);
     }
 }
